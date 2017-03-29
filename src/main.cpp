271be2bf75a4f01259ab918bd9139a0c5e2dc5f3
carolagille <i7679783@bournemouth.ac.uk>
2017-03-29 17:19:30 +0100

    make it cross platform

--- src/main.cpp
+++ src/main.cpp
@@ -11,20 +11,21 @@
 
 #include <QApplication>
 
-
+#include <QtGlobal>
 #include "GUI.h"
 
 int main(int argc, char *argv[])
 {
 
-
+    #ifdef Q_OS_MACX
     QSurfaceFormat format;
     format.setDepthBufferSize(32);
     format.setMajorVersion(4);
     format.setMinorVersion(1);
     format.setProfile(QSurfaceFormat::CoreProfile);
-
     QSurfaceFormat::setDefaultFormat(format);
+    #endif
+
 
 
   QApplication app(argc, argv);
