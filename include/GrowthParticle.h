1c2a98070b1fcbb566ef7e1c591796b09883c000
Esme Prior <i7628816@bournemouth.ac.uk>
2017-05-12 11:54:22 +0100

    Fixed issue with passing in particle centre position without needed.

--- include/GrowthParticle.h
+++ include/GrowthParticle.h
@@ -58,7 +58,7 @@ public:
   //////////////////////////////////////////////////////////////////////////////
   /// @brief Does not do anything for this class.
   //////////////////////////////////////////////////////////////////////////////
-  void calculate(QVector3D _particleCentre, std::vector<std::unique_ptr<Particle>> &_particleList, QVector3D _averageDistance,
+  void calculate(std::vector<std::unique_ptr<Particle>> &_particleList, QVector3D _averageDistance,
                  unsigned int _particleCount, QVector3D _lightPos, int _cohesionFactor, int _localCohesionFactor,
                  bool _particleDeath, int _automataRadius, int _automataTime) override;
 
--- include/LinkedParticle.h
+++ include/LinkedParticle.h
@@ -59,7 +59,6 @@ public:
   //////////////////////////////////////////////////////////////////////////////
   /// @brief Calculates the new velocity of the particle based on the forces
   /// that act on it.
-  /// @param _particleCentre Position of the average centre of all particles
   /// @param [in] _particleList List of all particles
   /// @param [in] _averageDistance Average distance between particles
   /// @param [in] _particleCount Total number of particles in the system
@@ -70,7 +69,7 @@ public:
   /// @param [in] _automataRadius Controls the radius in which automata are created
   /// @param [in] _automataTime Controls the speed at which automata are created
   //////////////////////////////////////////////////////////////////////////////
-  void calculate(QVector3D _particleCentre, std::vector<std::unique_ptr<Particle> > &_particleList, QVector3D _averageDistance,
+  void calculate(std::vector<std::unique_ptr<Particle> > &_particleList, QVector3D _averageDistance,
                  unsigned int _particleCount,QVector3D _lightPos, int _cohesionFactor, int _localCohesionFactor,
                  bool _particleDeath, int _automataRadius, int _automataTime) override;
 
@@ -88,7 +87,8 @@ public:
   void bulge(QVector3D _particleCentre) override;
 
   //////////////////////////////////////////////////////////////////////////////
-  /// @brief Adds food to random particles to create interesting effects..
+  /// @brief Adds food to random particles to create interesting effects.
+  /// @param [in] _particleCentre Position of the average centre of all particles
   //////////////////////////////////////////////////////////////////////////////
   void addFood(QVector3D _particleCentre) override;
 
